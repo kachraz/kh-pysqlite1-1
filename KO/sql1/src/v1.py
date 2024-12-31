5e44061fb3e351f481c02d72b2a099713d9d3c85
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 04:26:49 +0000

    🫦

--- KO/sql1/src/v1.py
+++ KO/sql1/src/v1.py
@@ -10,6 +10,9 @@ app = FastAPI()
 DATABASE_URL = "sqlite:///./pusy.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 def v1a():
