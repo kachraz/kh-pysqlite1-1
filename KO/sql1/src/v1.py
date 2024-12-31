76e1854fc49891fca6448346b7cb9c11b44bdf2d
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 04:24:46 +0000

    🫦

--- KO/sql1/src/v1.py
+++ KO/sql1/src/v1.py
@@ -1,4 +1,7 @@
 # Version1 Testing
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 def v1a():
