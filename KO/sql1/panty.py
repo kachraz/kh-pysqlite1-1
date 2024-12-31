f961eb7e6d481202ffe177fe342bd7fa52fe17cb
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 04:55:43 +0000

    🫦

--- KO/sql1/panty.py
+++ KO/sql1/panty.py
@@ -3,7 +3,7 @@ from rich import print as rprint
 from rich.traceback import install
 from src.uti import label
 from fastapi import FastAPI
-from db import app, DATABASE_URL, engine, SessionLocal, Base
+from src.v1 import app, DATABASE_URL, engine, SessionLocal, Base
 
 install(show_locals=True)
 
