f721c9048ef569b6ef3ce42d6a69aa368e37633a
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 05:18:04 +0000

    🫦

--- KO/sql1/panty.py
+++ KO/sql1/panty.py
@@ -3,7 +3,7 @@ from rich.traceback import install
 from src.uti import label
 from fastapi import FastAPI
 from src.v1 import my_db_pussy, DATABASE_URL, engine, SessionLocal, Base
-
+import uvicorn
 
 install(show_locals=True)
 
