7630a1549e2d9b450e76a4cb8dca25bf38e62948
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 04:23:17 +0000

    🫦

--- KO/sql1/panty.py
+++ KO/sql1/panty.py
@@ -1,12 +1,13 @@
 from src.v1 import v1a
 from rich import print as rprint
 from rich.traceback import install
+from src.uti import label
 
 install(show_locals=True)
 
 
 def main():
-    rprint("[blue] Running sv1a [/blue]")
+    label("Execute v1a")
     v1a()
 
 
